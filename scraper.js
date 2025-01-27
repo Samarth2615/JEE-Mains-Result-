87894b9c364a1ee341b274a895497669ec8810f2
Samarth2615 <164867341+Samarth2615@users.noreply.github.com>
2025-01-27 20:44:04 +0530

    Update scraper.js

--- scraper.js
+++ scraper.js
@@ -1,64 +1,16 @@
-const tables = document.querySelectorAll(".menu-tbl");
-const data = {};
+export default async function parseHTML(file) {
+  const text = await file.text();
+  const parser = new DOMParser();
+  const doc = parser.parseFromString(text, "text/html");
 
-tables.forEach((table, i) => {
-    const qnType = table.querySelectorAll("tr td.bold")[0].innerText.trim(); // "MCQ || "SA"
-    const qnID = table.querySelectorAll("tr td.bold")[1].innerText.trim();
+  const data = {};
+  doc.querySelectorAll(".menu-tbl").forEach((table) => {
+    const qnID = table.querySelector("tr td.bold:nth-child(2)").innerText.trim();
+    const hasAnswered = table.querySelector("tr td.bold:nth-child(8)").innerText.trim() !== "--";
+    const ownAnswer = hasAnswered ? table.querySelector("tr td.bold:nth-child(8)").innerText.trim() : null;
 
-    if (qnType === "MCQ") {
-        let hasAnswered = false;
-        const status2 = table
-            .querySelectorAll("tr td.bold")[7]
-            .innerText.trim();
+    data[qnID] = { hasAnswered, ownAnswer };
+  });
 
-        if (status2 !== "--") {
-            hasAnswered = true;
-        }
-
-        if (hasAnswered) {
-            let options = [0, 0, 0, 0];
-            for (let j = 0; j < 4; j++) {
-                options[j] = Number(
-                    table.querySelectorAll("tr td.bold")[2 + j].innerText
-                );
-            }
-
-            const ownAnswerIndex =
-                Number(
-                    table.querySelectorAll("tr td.bold")[7].innerText.trim()
-                ) - 1;
-            const ownAnswer = options[ownAnswerIndex];
-
-            data[qnID] = {
-                qnType,
-                hasAnswered,
-                options,
-                ownAnswer,
-            };
-        } else {
-            data[qnID] = { qnType, hasAnswered };
-        }
-    } else if (qnType === "SA") {
-        let hasAnswered = false;
-        const ownAnswer = document
-            .querySelectorAll(".questionRowTbl")
-            [i].querySelector("tr:nth-of-type(5) td.bold").innerText;
-
-        if (ownAnswer !== "--") {
-            hasAnswered = true;
-        }
-
-        if (hasAnswered) {
-            data[qnID] = {
-                qnType,
-                hasAnswered,
-                ownAnswer,
-            };
-        } else {
-            data[qnID] = { qnType, hasAnswered };
-        }
-    }
-});
-
-const generatedJSON = JSON.stringify(data, null, 2);
-console.log(generatedJSON);
+  return data;
+}
